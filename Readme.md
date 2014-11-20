2a223f08ee03ebff369507de997e637a832f319d
Sylvain Hallé <shalle@acm.org>
2014-11-20 18:53:07 -0500

    Initial version

---
+++ Readme.md
@@ -0,0 +1,152 @@
+A Flexible LaTeX Article Environment
+====================================
+
+This repository provides a boilerplate environment for writing LaTeX
+articles using the popular templates from Springer, IEEE and ACM. It
+provides:
+
+- The up-to-date style and bibliography files of all three publishers
+- A script that generates the proper preamble (title, list of authors and
+  institution) specific to each style
+- A very advanced Makefile (by [Chris
+  Monson](https://github.com/shiblon/latex-makefile)) taking care of the
+  compilation/cleaning process
+- A `.gitignore` file suitable for a LaTeX project
+
+Using this template, switching a paper from any stylesheet to any other
+simply amounts to selecting one line to de-comment in the main, `paper.tex`
+document.
+
+Why this template?
+------------------
+
+If you have been writing lots of Computer Science papers, you may have
+been mostly using LaTeX with three different document classes:
+
+- `llncs` for Springer' Lecture Notes in Computer Science series
+- `sig-alternate` for ACM conference proceedings
+- `IEEEtran` for IEEE conference proceedings and journals
+
+First off, this repository provides a well-structured template project where
+all these classes are included, so you can pick the one you wish when
+starting to write. Moreover, it comes with a very powerful Makefile that
+does all sorts of nifty things, such as suppressing useless output from
+LaTeX and colouring (yes, colouring) its meaningful output (errors in red,
+etc.).
+
+Moreover, there might be various reasons for which you might want to switch
+an existing document from one class to the other. For example, you started
+writing a paper without deciding where to send it, only to find that the
+conference you've chosen has a different publisher than the paper's current
+style. Or, a paper sent to a conference (and perhaps rejected) needs to be
+sent to another venue with a different publisher.
+
+Alas, it turns out these stylesheets are not directly interchangeable.
+Rather than nicely overriding the behaviour of LaTeX's original commands
+from the `article` document class, each class decided to invent its own
+commands to, e.g., define the title, authors and institution of a document
+--and none of them works the same way. For example, here is how to declare
+authors and institutions in `llncs`:
+
+    \author{Emmett Brown\inst{1} \and Marty McFly\inst{1} \and Biff Tannen\inst{2}}
+    \institute{%
+    Temporal Industries \\
+    Hill Valley, CA 90193 \\
+    \and
+    BiffCo inc. \\
+    Hill Valley, CA 90193 \\
+    }
+
+...in `IEEEtran`:
+
+    \author{%
+    \IEEEauthorblockN{Emmett Brown, Marty McFly}
+    \IEEEauthorblockA{%
+    Temporal Industries\\
+    Hill Valley, CA 90193\\
+    }
+    \IEEEauthorblockN{Biff Tannen}
+    \IEEEauthorblockA{%
+    BiffCo inc.\\
+    Hill Valley, CA 90193\\
+    }
+    }
+
+...and in `sig-alternate`:
+
+    \numberofauthors{2}
+    \author{%
+    \alignauthor Emmett Brown, Marty McFly\\
+    \affaddr{Temporal Industries} \\
+    \affaddr{Hill Valley, CA 90193} \\
+    \alignauthor Biff Tannen\\
+    \affaddr{BiffCo inc.} \\
+    \affaddr{Hill Valley, CA 90193} \\
+    }
+
+Three different sets of commands and syntax for the same data, and all this
+while `article.cls` already provides commands doing exactly that which could
+have easily been overridden! Therefore, switching between classes requires
+some amount of braindead, yet frustrating copy-pasting from existing files
+you have, which arguably becomes quite mind-numbing when you've been doing
+that once in a while for the past ten years.
+
+In this project, the paper's title, authors and institutions is written in
+a separate file called `authors.txt`:
+
+    Applications of the Flux Capacitor
+    
+    Emmett Brown (1)
+    Marty McFly (1)
+    Biff Tannen (2)
+    
+    1
+    Temporal Industries
+    Hill Valley, CA 95420
+    
+    2
+    BiffCo inc.
+    Hill Valley, CA 95420
+
+You then call a script named `generate-preamble.php` to generate include
+files with the proper syntax for each of the document classes from that
+same data. These files are called `preamble-xxxx.inc.tex`, where xxxx is
+the document class.
+
+In the he main paper file, called `paper.tex`, it suffices to uncomment the
+`\input` line for the desired preamble and compile:
+
+    %\input preamble-ieee.inc.tex
+    %\input preamble-lncs.inc.tex
+    \input preamble-acm.inc.tex
+
+To change the authors or title, modify `article.tex` and call
+`generate-preamble.php` again. To switch between document classes, select
+another `\input` line to uncomment and recompile. Voilà!
+
+Quick Use
+---------
+
+0. Unzip (or clone) the contents of this repository in a folder of your
+   choice.
+
+1. Modify `article.txt` with the desired title, authors and institutions.
+   The file is self-document and tells you how to do it.
+
+2. Call `php generate-preamble.php` to generate the include files, which
+   will be placed in the `Source` subfolder.
+
+3. Write your text as usual in `Source/paper.tex`. Uncomment the `\input`
+   line corresponding to the document class you wish to use. Figures should
+   be placed in the `fig` subfolder.
+
+4. To compile, use `make all`. To remove temporary files, use `make clean`.
+   The Makefile has a very comprehensive list of other useful features. To
+   read them, run `make help`.
+
+About the Author
+----------------
+
+This project is maintained by [Sylvain Hallé](http://leduotang.ca/sylvain),
+Associate Professor at [Université du Québec à
+Chicoutimi](http://www.uqac.ca), Canada.
---
+++ generate-preamble.php
@@ -0,0 +1,291 @@
+<?php
+
+// Read author-title file
+$input_filename = "authors.txt";
+$out_folder = "Source/";
+$lines = explode("\n", file_get_contents($input_filename));
+
+{ // Parse file {{{
+  $has_title = false;
+  $current_affiliation = 1;
+  $title = "";
+  $affiliations = array();
+  $authors = array();
+  foreach ($lines as $line)
+  { // {{{
+    $line = trim($line);
+    if (empty($line) || $line[0] === "#")
+    {
+      continue;
+    }
+    if (!$has_title)
+    {
+      $title = $line;
+      $has_title = true;
+      continue;
+    }
+    $matches = array();
+    if (preg_match("/^(.*)\\((\\d+)\\)$/", $line, $matches))
+    {
+      $authors[trim($matches[1])] = trim($matches[2]);
+    }
+    elseif (preg_match("/^\\d+$/", $line))
+    {
+      $current_affiliation = $line;
+    }
+    else
+    {
+      if (!isset($affiliations[$current_affiliation]))
+      {
+        $affiliations[$current_affiliation] = array();
+      }
+      $affiliations[$current_affiliation][] = $line;
+    }
+  }
+} // }}}
+
+// Now that authors and affiliations are known, generate the preamble
+// specific to each stylesheet
+
+{ // Springer LNCS {{{
+  
+  // Preamble
+  $out = "";
+  $out .= <<<EOD
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This is an auto-generated file. DO NOT EDIT!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\documentclass{llncs}
+
+% Usual packages
+\usepackage[utf8]{inputenc}  % UTF-8 input encoding
+\usepackage[T1]{fontenc}     % Type1 fonts
+\usepackage{lmodern}         % Improved Computer Modern font
+\usepackage{microtype}       % Better handling of typo
+\usepackage[english]{babel}  % Hyphenation
+\usepackage{graphicx}        % Import graphics
+\usepackage{cite}            % Better handling of citations
+\usepackage{hyperref}        % Better handling of references in PDFs
+\usepackage{comment}         % To comment out blocks of text
+EOD;
+
+  $out .= "\n\n% Title\n";
+  $out .= "\\title{".$title."}\n\n";
+  
+  $out .= "% Authors and affiliations\n";
+  $out .= "\\author{";
+  $first = true;
+  foreach ($authors as $name => $aff)
+  {
+    if ($first)
+    {
+      $first = false;
+    }
+    else
+    {
+      $out .= " \\and ";
+    }
+    $out .= $name."\\inst{".$aff."}";
+  }
+  $out .= "}\n";
+  $out .= "\\institute{%\n";
+  $first = true;
+  foreach ($affiliations as $key => $lines)
+  {
+    if ($first)
+    {
+      $first = false;
+    }
+    else
+    {
+      $out .= "\\and\n";
+    }
+    foreach ($lines as $line)
+    {
+      $out .= $line." \\\\\n";
+    }
+  }
+  $out .= "}\n";
+  file_put_contents($out_folder."preamble-lncs.inc.tex", $out);
+  
+  // Postamble
+  $out = "";
+  $out .= <<<EOD
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This is an auto-generated file. DO NOT EDIT!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+EOD;
+  $out .= "\n\\bibliographystyle{splncs03}\n";
+  $out .= "\\bibliography{paper}\n";
+  file_put_contents($out_folder."postamble-lncs.inc.tex", $out);
+} // }}}
+
+{ // IEEE Conference Proceedings {{{
+  
+  // Preamble
+  $out = "";
+  $out .= <<<EOD
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This is an auto-generated file. DO NOT EDIT!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\documentclass[conference]{IEEEtran}
+
+% Usual packages
+\usepackage[utf8]{inputenc}  % UTF-8 input encoding
+\usepackage[T1]{fontenc}     % Type1 fonts
+\usepackage{mathptmx}        % Times with math support
+\usepackage{microtype}       % Better handling of typo
+\usepackage[english]{babel}  % Hyphenation
+\usepackage{graphicx}        % Import graphics
+\usepackage{cite}            % Better handling of citations
+\usepackage{hyperref}        % Better handling of references in PDFs
+\usepackage{comment}         % To comment out blocks of text
+EOD;
+
+  $out .= "\n\n% Title\n";
+  $out .= "\\title{".$title."}\n\n";
+  
+  // Group all authors with same affiliation
+  $authors_aff = array();
+  foreach ($authors as $name => $aff)
+  {
+    if (!isset($authors_aff[$aff]))
+    {
+      $authors_aff[$aff] = array();
+    }
+    $authors_aff[$aff][] = $name;
+  }
+  $out .= "% Authors and affiliations\n";
+  $out .= "\\author{%\n";
+  foreach ($authors_aff as $aff => $names)
+  {
+    $first = true;
+    $out .= "\\IEEEauthorblockN{";
+    foreach ($names as $name)
+    {
+      if ($first)
+      {
+        $first = false;
+      }
+      else
+      {
+        $out .= ", ";
+      }
+      $out .= $name;
+    }
+    $out .= "}\n";
+    $out .= "\\IEEEauthorblockA{%\n";
+    foreach ($affiliations[$aff] as $line)
+    {
+      $out .= $line."\\\\\n";
+    }
+    $out .= "}\n";
+  }
+  $out .= "}\n";
+  file_put_contents($out_folder."preamble-ieee.inc.tex", $out);
+  
+  // Postamble
+  $out = "";
+  $out .= <<<EOD
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This is an auto-generated file. DO NOT EDIT!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+EOD;
+  $out .= "\n\\bibliographystyle{abbrv}\n";
+  $out .= "\\bibliography{paper}\n";
+  file_put_contents($out_folder."postamble-ieee.inc.tex", $out);
+} // }}}
+
+{ // ACM {{{
+  
+  // Preamble
+  $out = "";
+  $out .= <<<EOD
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This is an auto-generated file. DO NOT EDIT!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\documentclass{sig-alternate}
+\pdfpagewidth=8.5truein
+\pdfpageheight=11truein
+
+% Usual packages
+\usepackage[utf8]{inputenc}  % UTF-8 input encoding
+\usepackage[T1]{fontenc}     % Type1 fonts
+\usepackage{lmodern}         % Improved Computer Modern
+\usepackage{microtype}       % Better handling of typo
+\usepackage[english]{babel}  % Hyphenation
+\usepackage{graphicx}        % Import graphics
+\usepackage{cite}            % Better handling of citations
+\usepackage{hyperref}        % Better handling of references in PDFs
+\usepackage{comment}         % To comment out blocks of text
+
+% Fix ACM's awkward 2-column justification rules
+\sloppy
+
+% Paper Metadata
+\conferenceinfo{OUTATIME'55,} {November 5--12, 1955, Hill Valley, CA.}
+\CopyrightYear{1955}
+\crdata{978-1-4503-0116-9/10/09}
+\clubpenalty=10000
+\widowpenalty = 10000
+EOD;
+
+  $out .= "\n\n% Title\n";
+  $out .= "\\title{".$title."}\n\n";
+  
+  // Group all authors with same affiliation
+  $authors_aff = array();
+  foreach ($authors as $name => $aff)
+  {
+    if (!isset($authors_aff[$aff]))
+    {
+      $authors_aff[$aff] = array();
+    }
+    $authors_aff[$aff][] = $name;
+  }
+  $out .= "% Authors and affiliations\n";
+  $out .= "\\numberofauthors{".count($affiliations)."}\n";
+  $out .= "\\author{%\n";
+  foreach ($authors_aff as $aff => $names)
+  {
+    $first = true;
+    $out .= "\\alignauthor ";
+    foreach ($names as $name)
+    {
+      if ($first)
+      {
+        $first = false;
+      }
+      else
+      {
+        $out .= ", ";
+      }
+      $out .= $name;
+    }
+    $out .= "\\\\\n";
+    foreach ($affiliations[$aff] as $line)
+    {
+      $out .= "\\affaddr{".$line."} \\\\\n";
+    }
+  }
+  $out .= "}\n";
+  file_put_contents($out_folder."preamble-acm.inc.tex", $out);
+  
+  // Postamble
+  $out = "";
+  $out .= <<<EOD
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This is an auto-generated file. DO NOT EDIT!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+EOD;
+  $out .= "\n\\bibliographystyle{abbrv}\n";
+  $out .= "\\bibliography{paper}\n";
+  $out .= "\\balancecolumns\n";
+  file_put_contents($out_folder."postamble-acm.inc.tex", $out);
+} // }}}
+
+// :wrap=none:folding=explicit:
+?>
