f2a2bafcd92b5e9bb641178e4c6ad42c947c9523
Sylvain Hallé <shalle@acm.org>
2015-01-14 11:49:30 -0500

    Added GPL comment + fixed folding

--- generate-preamble.php
+++ generate-preamble.php
@@ -1,4 +1,21 @@
 <?php
+/**************************************************************************
+  A Flexible LaTeX Article Environment
+  Copyright (C) 2015-2015  Sylvain Hallé
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************************/
 
 // Read author-title file
 $input_filename = "authors.txt";
@@ -294,6 +311,8 @@ EOD;
   $out .= "\\balancecolumns\n";
   file_put_contents($out_folder."postamble-acm.inc.tex", $out);
 } // }}}
+
+// }}}
 
 // :wrap=none:folding=explicit:
 ?>
